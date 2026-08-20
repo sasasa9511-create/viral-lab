ec602c19a20aaa8c173340811968584441d1bbc3
sasasa9511-create <sasasa9511@gmail.com>
2026-08-20 08:21:46 +0330

    feat: expose operational video analysis route

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
-import "./styles.css";
+import AnalysisPage from "./AnalysisPage";
+import "../styles.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function RoutedApp() {
+  const path = window.location.pathname.replace(/\/+$/, "") || "/";
+  if (path === "/analysis") {
+    return <AnalysisPage user={null} onToast={() => {}} />;
+  }
+  return <App />;
+}
+
+root.render(
   <React.StrictMode>
-    <App />
+    <RoutedApp />
   </React.StrictMode>
 );
